585a835664073b316eb798433b0c9433024e78df
Felix Ayala <felix1262@gmail.com>
2015-12-18 16:28:56 -0300

    Initial Commit

---
+++ Patience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Patience
+//
+//  Created by Felix Ayala on 12/18/15.
+//  Copyright © 2015 Pandorga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
